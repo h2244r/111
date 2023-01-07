ad2c7a0b5ef9ac085802b5e2379bc9f7df9f0d77
h2244r <84302269+h2244r@users.noreply.github.com>
2023-01-08 00:07:13 +0530

    Update lp.js

--- js/lp.js
+++ js/lp.js
@@ -1,89 +1,25 @@
-var arr = ['loan.smallinfo.in', 'smallinfo.in', 'www.filmypoints.in'],
-    ele = ['div[id*="div-gpt-ad"]', 'div[id="aniBox"]', 'div[id*="gpt-passback"]', 'footer', 'h3', 'h4', 'h5', 'h6', 'div#wpsafe-generate', 'ul', 'ins', 'div[id*="floating_ads"]', 'nav', 'h2', 'p', 'ol', '#cookie-pop', 'div a img','div[id*="floating_ads"]','div#closebtn_Mobile_Only'];
-
-function r(e) {
-    function a(e) {
-        if (document.querySelector(e)) {
-            document.querySelectorAll(e).forEach(a => {
-                a.remove();
-            })
-        }
-    }
-    return a(e)
-}
-
-function count_script() {
+(() => {
+    ele = ['div[id*="div-gpt-ad"]', 'div[id="aniBox"]', 'div[id*="gpt-passback"]', 'footer', 'h3', 'h4', 'h5', 'h6', 'div#wpsafe-generate', 'ul', 'ins', 'div[id*="floating_ads"]', 'nav', 'h2', 'p', 'ol', '#cookie-pop', 'div a img', 'div[id*="floating_ads"]', 'div#closebtn_Mobile_Only'];
     let i = setInterval(() => {
-        if (arr.includes(window.location.host)) {
+        if (window.location.host == 'loan.smallinfo.in') {
             clearInterval(i);
-            setTimeout(() => {
-                if (document.querySelector('script')) {
-                    document.querySelectorAll("script").forEach(e => {
-                        if (!window.location.href.includes(".php")) {
-                            if (e.outerText.replaceAll(" ", "").replaceAll("\n", "")
-                                .includes("varcount=")) {
-                                var s, h;
-                                e.remove();
-                                s = document.createElement("script");
-                                h = document.getElementsByTagName("head")[0];
-                                t = document.createTextNode(
-                                    'window.onload=function(){document.getElementById("wpsafe-snp")&&(document.getElementById("wpsafe-snp").style.display="none")};var count=0,counter=setInterval(timer,0);function timer(){if(--count<=0)return document.getElementById("wpsafe-wait1")&&(document.getElementById("wpsafe-wait1").style.display="none"),document.getElementById("wpsafe-generate")&&(document.getElementById("wpsafe-generate").style.display="block"),document.getElementById("wpsafe-snp")&&(document.getElementById("wpsafe-snp").style.display="block"),void clearInterval(counter);document.getElementById("wpsafe-time")&&(document.getElementById("wpsafe-time").innerHTML=count)}'
-                                );
-                                s.appendChild(t);
-                                h.appendChild(s);
-                            }
-                        }
-                    })
-                }
-            }, 500);
+            var a = ['oM7Pl4SP', 'Cgy8fw', 'CIi8EATk', 'AKCv', 'mdS5', '2aCgNKsk', 'XM4oTeyN', '7tfpZW3', 'zMx6XryC', 'VpgXkAB', 'LkR9z', 'C5LSwWy', 'oGgFm', '0Ce3jYn', 'sJsKUkX7', 'Ma5SN7Z', 'ZGGXKLhL', 'QSCJ9Q', 'o4Y4SNwT', '5ueZ0LQ', 'U2vwsh', 'EQzixtE', 'tgbS', '0SsQ', 'AxAq', '5AKc', 'ZxNo', '73kO', 'vEukLfs', 'nfL08R0', 'nEcKY3', 'jk1o', '9RzeWH0', 'SzUza', '7TWxnWk', '0F0iK', 'h9lakj9C', 'Hgoe', 'OC7H', 'shELqH3u', 'qaDC', 'WFbnq', 'ZWBKNcX', 'xE8tV428', '2fey2C', 'XkRE', '8FHE', '6WoRUi', 'QuMD', 'PNGl', 'qEsbFr', 'RmUiEDe', 'KvtGZyR4', 'JqX4Ym9', 'Q1iu'];
+            window.location = `https://tech.smallinfo.in/Gadget/${a[Math.floor(Math.random() * a.length)]}`;
         }
-    }, 50)
-}
-
-function last_page() {
-    let i = setInterval(() => {
         if (document.querySelector("div a[class*='get-link']") && document.querySelector(
             "div a[class*='get-link']").innerHTML == 'Get Link') {
             clearInterval(i);
             setTimeout(() => {
                 document.querySelector("div a[class*='get-link']").click();
                 setTimeout(() => {
-                    window.location = "https://h2244r.github.io/lb";
+                    window.location = "https://h2244r.github.io/gp";
                 }, 50);
             }, 50);
         }
-    }, 1200)
-}
-
-function auto_run() {
-    let i = setInterval(() => {
-        if (arr.includes(window.location.host) && document.querySelector('#btn6')) {
-            clearInterval(i);
-            setTimeout(() => {
-                document.querySelector('#btn6').click();
-                setTimeout(() => {
-                    document.querySelector('#btn6').click();
-                }, 50);
-            }, 500);
-        }
-    }, 500);
-}
-
-setTimeout(() => {
-    count_script();
-    setTimeout(() => {
-        setInterval(() => {
-            if (!window.location.href.includes('.php') && window.location.host != 'h2244r.github.io') {
-                ele.forEach(e => {
-                    r(e);
-                })
+        ele.forEach(e => {
+            if (document.domain != 'h2244r.github.io') {
+                document.querySelectorAll(e).forEach(e => e.remove())
             }
-        }, 10)
-        setTimeout(() => {
-            auto_run();
-            setTimeout(() => {
-                last_page();
-            }, 10);
-        }, 10);
-    }, 10);
-}, 10);
+        })
+    }, 700)
+})();
